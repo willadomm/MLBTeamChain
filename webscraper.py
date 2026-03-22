f7da93090a129ea343fd69d194a961b7a18ea08d
willadomm <wdomm@oberlin.edu>
2026-03-22 00:32:26 -0400

    for sure cooking

--- webscraper.py
+++ webscraper.py
@@ -1,42 +1,70 @@
 import re
 import json
+import time
 import requests
 from bs4 import BeautifulSoup
 
-page = requests.get("https://www.retrosheet.org/boxesetc/2024/UANA02024.htm")
 
-soup = BeautifulSoup(page.text, 'html.parser')
+"""
+Problems!
+1. Intitals in the data set, probably manually needs fixing
+
+"""
+
+invalidyears = []
+datalist = []
+for year in range(2013, 2014):
+    url = f"https://www.retrosheet.org/boxesetc/{year}/UANA0{year}.htm"
+    try:
+        page = requests.get(url)
+    except:
+        invalidyears.append(year)
+        continue
+
+    time.sleep(1)
+
+    soup = BeautifulSoup(page.text, 'html.parser')
+    
 
 
-roster = soup.find_all('a')
+    roster = soup.find_all('a')
 
-roster = roster[52:]
-
-
-playersarray = []
-
-for pre in roster:
-   firstnamelastnamearray = pre.get_text().split()
-   
-
-   if len(firstnamelastnamearray) > 1:
-        playername = str(firstnamelastnamearray[0] + " " + firstnamelastnamearray[1] )
-        playersarray.append(playername)
+    roster = roster[52:]
 
 
-playersarray = list(set(playersarray))
+    playersarray = []
 
-playersarray.sort()
-playersarray.remove("Read Me")
-print(playersarray)
+    for pre in roster:
+        firstnamelastnamearray = pre.get_text().split()
+        
 
-data = {
-    "team": "ANA 2024",
-    "players": playersarray
-}
+        playername = ""
+
+        if len(firstnamelastnamearray) > 1:
+            for name in firstnamelastnamearray:
+                playername = playername + str(name + " ")
+            playersarray.append(playername)
+
+
+    playersarray = list(set(playersarray))
+
+    playersarray.sort()
+
+    
+    if "Read Me" in playersarray:
+        playersarray.remove("Read Me")
+
+
+    data = {
+            "team": f"ANA {year}",
+            "players": playersarray
+    }
+    datalist.append(data)
+
+
 
 with open ("data.json", "w") as file:
-    json.dump(data, file)
+    json.dump(datalist, file, indent=2)
 
    
 
