5f73340c855c172b9a368975e3ac03dd9b2721dc
willadomm <wdomm@oberlin.edu>
2026-03-23 00:25:14 -0400

    almost done w the data collection

--- webscraper.py
+++ webscraper.py
@@ -5,65 +5,98 @@ import requests
 from bs4 import BeautifulSoup
 
 
-"""
-Problems!
-1. Intitals in the data set, probably manually needs fixing
+
 
 """
+teamidentifiers = ["LAA", "CAL", "BSN", "NY1", "BRO", "BLN", "MLN", "FLO", "MON", "HOU", "ARI", "ATL", "CHN", 
+                   "CIN", "COL", "LAN", "SDN", "MIA", "MIL", "NYN", "PHI", "PIT", "SFN", "SLN", "WAS"
+                   "PHA", "WS1", "SLA", "KC1", "SE1", "WS2", "BAL", "BOS", "CHA", "CLE", "DET", "HOU"
+                   "KCA", "MIN", "NYA", "OAK", "TBA", "TEX", "TOR", "ATH"]
+"""
+
+teamidentifiers = ["ANA"]
 
 invalidyears = []
+abbreviatednames = []
 datalist = []
-for year in range(2013, 2014):
-    url = f"https://www.retrosheet.org/boxesetc/{year}/UANA0{year}.htm"
-    try:
-        page = requests.get(url)
-    except:
-        invalidyears.append(year)
-        continue
+for team in teamidentifiers:
 
-    time.sleep(1)
+    
 
-    soup = BeautifulSoup(page.text, 'html.parser')
+    for year in range(1900, 2026):
+        print(year)
+        url = f"https://www.retrosheet.org/boxesetc/{year}/U{team}0{year}.htm"
+        try:
+            page = requests.get(url)
+        except:
+            invalidyears.append(year)
+            continue
+
+        time.sleep(1)
+
+        soup = BeautifulSoup(page.text, 'html.parser')
     
 
 
-    roster = soup.find_all('a')
+        roster = soup.find_all('a')
 
-    roster = roster[52:]
+        roster = roster[52:]
 
 
-    playersarray = []
+        playersarray = []
 
-    for pre in roster:
-        firstnamelastnamearray = pre.get_text().split()
+        for pre in roster:
+            firstnamelastnamearray = pre.get_text().split()
         
 
-        playername = ""
+            playername = ""
 
-        if len(firstnamelastnamearray) > 1:
-            for name in firstnamelastnamearray:
-                playername = playername + str(name + " ")
-            playersarray.append(playername)
+            
+
+            if len(firstnamelastnamearray) > 1:
+                for name in firstnamelastnamearray:
+                    playername = playername + str(name + " ")
+                
+                if "." in playername:
+                    stringurl = str(pre.get("href"))
+                    stringurl = stringurl[2:]
+                    newlink = "https://www.retrosheet.org/boxesetc"+stringurl
+                    playerpage = requests.get(newlink)
+                    playersoup = BeautifulSoup(playerpage.text, 'html.parser')
+                    name = playersoup.find('h2')
+                    playername = name.get_text()
+                
+                
+                
+                playersarray.append(playername)
+
+            
 
 
-    playersarray = list(set(playersarray))
+        playersarray = list(set(playersarray))
 
-    playersarray.sort()
+        playersarray.sort()
 
+
+        if "Read Me" in playersarray:
+            playersarray.remove("Read Me")
     
-    if "Read Me" in playersarray:
-        playersarray.remove("Read Me")
+        if "Read Me " in playersarray:
+            playersarray.remove("Read Me ")
 
 
-    data = {
-            "team": f"ANA {year}",
-            "players": playersarray
-    }
-    datalist.append(data)
+        data = {
+                "team": f"{team} {year}",
+                "players": playersarray
+        }
+        
+        if len(playersarray) > 0:
+            datalist.append(data)
 
 
+filestring = f"{team}.json"
 
-with open ("data.json", "w") as file:
+with open ("json data/" + filestring, "w") as file:
     json.dump(datalist, file, indent=2)
 
    
